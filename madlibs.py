8dfd2d2099ec716806cac7d86b87681599f51fe6
roam-git <85209337+roam-git@users.noreply.github.com>
2021-06-01 21:40:23 -0700

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,35 @@
-def lorax():
-    print("lorax")
+def beemovie():
+    print("nouns")
+    aviation = 
+    wing = 
+    bee = 
+    body = 
+    human = 
+    graduate = 
+    stair = 
+    lint = 
+    fuzz = 
+    hothead = 
+    hive = 
+    mustache = 
+    squirrel = 
+    amusementpark = 
+    vacation = 
+    print("colors")
+    yellow = 
+    black = 
+    print("numbers")
+    three = 
+    print("names")
+    barry = 
+    adam = 
+    frankie = 
+    deanbuzzwell = 
+    print("verbs")
+    fly = 
+    hitchhik = 
+    sting = 
+    print (
     return
 
 
