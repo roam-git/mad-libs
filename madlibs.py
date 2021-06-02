b2e7ec1078bffb1475f6f4737ee9c6917bf7b80e
Gopherslol <49794455+Gopherslol@users.noreply.github.com>
2021-06-01 21:43:25 -0700

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -10,7 +10,7 @@ print("Mad Libs but not becuase copyright or something. idk.")
 print("1. The Lorax - In progress\n2. The Bee Movie\n3. Shrek")
 selection = input(" > ")
 
-if selection == 1:
+if selection == '1':
     lorax()
-elif selection == 2:
+elif selection == '2':
     beemovie()
