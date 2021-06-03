6308d9b02d35e74327597ac0d2f2db21f224d6d0
roam-git <85209337+roam-git@users.noreply.github.com>
2021-06-02 21:26:18 -0700

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -5,45 +5,6 @@ def lorax():
 def 1337():
     print("you're pretty 1337")
     return
-
-
-print("Mad Libs but not because copyright or something. idk.")
-print("1. The Lorax - In progress\n2. The Bee Movie\n3. Shrek\n4. Open Season (yeah idk either)\n5. Mary Had A Little Lamb")
-selection = input(" > ")
-
-if selection == '1':
-    lorax()
-elif selection == '2':
-    beemovie()
-elif selection == '1337':
-    1337()
-
-    #the below shit is just abunch of shit i've got to figure out how to properly implement to make the clear command work
-# import only system from os
-from os import system, name
-
-# import sleep to show output for some time period
-from time import sleep
-
-# define our clear function
-def clear():
-
-	# for windows
-	if name == 'nt':
-		_ = system('cls')
-
-	# for mac and linux(here, os.name is 'posix')
-	else:
-		_ = system('clear')
-
-
-# sleep for 2 seconds after printing output
-sleep(2)
-
-# now call function we defined above
-clear()
-
-
 def beemovie():
     print()
     print("Nouns")
@@ -94,3 +55,43 @@ def beemovie():
     #final madlib code dsplay code below code code dnba;eua;ejaeuihafkjheaiuhfkjbfdskjbsadfkjhasefkjhafsdjkhasdfkjhsdafkljhdsfhjkldsfakhjlfdsalhkji
     print()
     print("cock")
+
+
+
+print("Mad Libs but not because copyright or something. idk.")
+print("1. The Lorax - In progress\n2. The Bee Movie\n3. Shrek\n4. Open Season (yeah idk either)\n5. Mary Had A Little Lamb")
+selection = input(" > ")
+
+if selection == '1':
+    lorax()
+elif selection == '2':
+    beemovie()
+elif selection == '1337':
+    1337()
+
+    #the below shit is just abunch of shit i've got to figure out how to properly implement to make the clear command work
+# import only system from os
+from os import system, name
+
+# import sleep to show output for some time period
+from time import sleep
+
+# define our clear function
+def clear():
+
+	# for windows
+	if name == 'nt':
+		_ = system('cls')
+
+	# for mac and linux(here, os.name is 'posix')
+	else:
+		_ = system('clear')
+
+
+# sleep for 2 seconds after printing output
+sleep(2)
+
+# now call function we defined above
+clear()
+
+
